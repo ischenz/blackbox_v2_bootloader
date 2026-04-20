ddb8395152eb8acd364952810960403692079d30
ischen.x <ischen.x@foxmail.com>
2026-04-20 23:44:15 +0800

    添加app编译时间打印，添加fh_bl_firmware_packet_t结构体，方便解析固件传输帧数据 解决固件大小计算错误导致的crc校验错误bug 添加一些打印信息

--- fh_bootloader/fh_bootloader.c
+++ fh_bootloader/fh_bootloader.c
@@ -9,6 +9,7 @@
 #include "fh_bootloader.h"
 #include "fh_stream.h"
 #include <string.h>
+#include <time.h>
 #include "fh_sw_crc.h"
 #include "ringbuff.h"
 #include "usart.h"
@@ -123,24 +124,39 @@ static int fh_bl_flash_write(uint8_t *data, uint16_t len)
 }
 
 // 传输完成后写入最后剩下的数据到flash
-static int fh_bl_flash_write_final(void)
+static int fh_bl_flash_write_final(size_t *app_size)
 {
+    int16_t write_len = 0; 
     if (write_ptr > 0) { // 如果缓冲区还有剩余数据，写入flash
-        uint16_t write_len = (write_ptr / 4) * 4 + 4; // 取4字节的整数倍长度，如果不足4字节，补齐到4字节
+        write_len = (write_ptr / 4) * 4 + 4; // 取4字节的整数倍长度，如果不足4字节，补齐到4字节
         if (stm32_flash_write(flash_write_ptr, write_buff, write_len) != 0) {
             return -1; // 写入flash失败
         }
-        flash_write_ptr += write_len; // 更新flash写入指针
-    }
+    } 
+    *app_size = flash_write_ptr  - FH_BL_APP_ADDR; // 计算APP大小
+    flash_write_ptr += write_len; // 更新flash写入指针
+    flash_write_ptr = FH_BL_APP_ADDR;   //重置FLASH写入指针
+    write_ptr = 0;                      //重置写入缓冲区指针
     return 0;
 }
 
 /*****************************************fh_bootloader 内部函数*********************************************** */
+typedef struct {
+    uint32_t pack_id; // 数据包ID，4字节
+    uint8_t data[];   // 数据内容，长度不固定
+} fh_bl_firmware_packet_t;
 
 static int fh_bl_crc_check(uint32_t app_addr, uint32_t app_size, uint32_t app_crc)
 {
+    int ret = 0;
     uint32_t crc = fh_sw_crc32((uint8_t *)app_addr, app_size);
-    return crc == app_crc ? 0 : -1; // 返回0表示校验通过，-1表示校验失败
+    if (crc == app_crc) {
+        FH_BL_PRINT("app crc check passed (%08lX)\r\n", crc);
+    } else {
+        FH_BL_PRINT("app crc check failed, expected %08lX but got %08lX\r\n", app_crc, crc);
+        ret = -1;
+    }
+    return ret; // 返回0表示校验通过，-1表示校验失败
 }
 
 static void fh_bl_info_read(fh_bl_info_t *info)
@@ -182,32 +198,27 @@ static int fh_bl_update(size_t *app_size)
     uint8_t mem[sizeof(fh_stream_frame_t) + (1 << sizeof(length_type) * 8)];
     fh_stream_frame_t *freame = (fh_stream_frame_t *)mem; 
     fh_bl_clear_app();                  //清除APP区域FLASH数据
-    static uint32_t pack_id = 0;
-    flash_write_ptr = FH_BL_APP_ADDR;   //重置FLASH写入指针
-    write_ptr = 0;                      //重置写入缓冲区指针
-    pack_id = 0;                        //重置数据包ID
-
+    static uint32_t pack_id_idx = 0;
+    pack_id_idx = 0;                        //重置数据包ID
     for (;;) {
         uint8_t buf;
         if (Read_RingBuff(&Uart1_RingBuff, &buf) == RINGBUFF_OK) {
             if (fh_stream_unpack(buf, freame) == FH_STREAM_EVENT_FRAME_RECEIVED) {
-                uint32_t id = 0;
+                fh_bl_firmware_packet_t *packet = (fh_bl_firmware_packet_t *)freame->value; // 把帧数据强制转换为固件数据包结构体指针，方便访问数据包ID和内容
                 switch (freame->tag) {
                     case FH_STREAM_TAG_DATA: // id在这里判断
-                        id = *(uint32_t *)freame->value;//前4字节是id
-                        if (id != pack_id || freame->length < 4) { // id不对，丢弃数据，
-                            FH_BL_PRINT("packet id mismatch, expected %lu but got %lu\r\n", pack_id, id);
+                        if (packet->pack_id != pack_id_idx || freame->length < sizeof(fh_bl_firmware_packet_t)) { // id不对，丢弃数据，
+                            FH_BL_PRINT("packet id mismatch, expected %lu but got %lu\r\n", pack_id_idx, packet->pack_id);
                             continue;
                         }
-                        fh_bl_flash_write(freame->value + 4, freame->length - 4);
-                        fh_bl_ack(pack_id); // 发送ACK
-                        pack_id++; // 数据包ID加1
+                        fh_bl_flash_write(packet->data, freame->length - sizeof(packet->pack_id)); // 写入数据到flash，去掉数据包ID的长度
+                        fh_bl_ack(pack_id_idx); // 发送ACK
+                        pack_id_idx++; // 数据包ID加1
                         break;
                     case FH_STREAM_TAG_CMD: // 接收结束，校验crc跳转到APP
-                        fh_bl_flash_write_final(); // 写入最后剩下的数据到flash
-                        if (fh_bl_crc_check(FH_BL_APP_ADDR, flash_write_ptr - FH_BL_APP_ADDR, *(uint32_t *)freame->value) == 0) { // 校验APP的CRC
+                        fh_bl_flash_write_final(app_size); // 写入最后剩下的数据到flash
+                        if (fh_bl_crc_check(FH_BL_APP_ADDR, *app_size, *(uint32_t *)freame->value) == 0) { // 校验APP的CRC
                             fh_bl_ack(0); // 发送升级成功ACK
-                            *app_size = flash_write_ptr - FH_BL_APP_ADDR; // 计算APP大小
                             return 0; // 升级成功
                         } else { // 校验失败，清除APP区域FLASH数据，重置写入指针，继续等待数据
                             FH_BL_PRINT("app crc check failed\r\n"); 
@@ -215,7 +226,7 @@ static int fh_bl_update(size_t *app_size)
                             fh_bl_ack(1); // 发送升级成功ACK
                             flash_write_ptr = FH_BL_APP_ADDR; //重置FLASH写入指针
                             write_ptr = 0;           //重置写入缓冲区指针
-                            pack_id = 0;             //重置数据包ID
+                            pack_id_idx = 0;             //重置数据包ID
                             return -1; 
                         }
                         break;
@@ -230,11 +241,12 @@ static int fh_bl_update(size_t *app_size)
 
 static void fh_bl_hello(fh_bl_info_t *info)
 {
+    time_t t = (time_t)(info->app_build_time);
     FH_BL_PRINT("=================fh bootloader v1.0=====================\r\n");
     FH_BL_PRINT("bootloader version: %lx\r\n", info->boot_version);
     FH_BL_PRINT("upgrade flag: %lu\r\n", info->upgrade_flag);
     FH_BL_PRINT("app version: %lx\r\n", info->app_version);
-    FH_BL_PRINT("app build time: %lu\r\n", info->app_build_time);
+    FH_BL_PRINT("app build time: %s\r\n", ctime(&t));
     FH_BL_PRINT("app size: %lu B\r\n", info->app_size);
     FH_BL_PRINT("boot count: %lu\r\n", info->boot_count);
     FH_BL_PRINT("=========================================================\r\n");
@@ -286,6 +298,7 @@ void fh_bl_boot(void)
             // 处理升级失败的情况 软重启
             NVIC_SystemReset();
         }
+        FH_BL_PRINT("app upgrade success, size: %u B\r\n", app_size);
         bl_info.app_size = app_size; // 设置app大小
         bl_info.upgrade_flag = 0; // 下载固件后清除升级标志
         fh_bl_info_write(&bl_info);
